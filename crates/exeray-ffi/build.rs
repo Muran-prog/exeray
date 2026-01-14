d636c370e6b7885017cf0b04cd1a57d12ccf3b3d
Muran-prog <fforgg69@gmail.com>
2026-01-14 20:03:59 +0200

    fix(ffi): Add spdlog library to Rust linker paths
    Link spdlog static library to fix undefined symbol error for
    spdlog::spdlog_ex typeinfo when building Rust FFI.

--- crates/exeray-ffi/build.rs
+++ crates/exeray-ffi/build.rs
@@ -2,7 +2,13 @@ fn main() {
     let dst = cmake::Config::new("../../core").profile("Release").build();
 
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    // spdlog is built in CMake's _deps subdirectory
+    println!(
+        "cargo:rustc-link-search=native={}/build/_deps/spdlog-build",
+        dst.display()
+    );
     println!("cargo:rustc-link-lib=static=exeray_core");
+    println!("cargo:rustc-link-lib=static=spdlog");
 
     cxx_build::bridge("src/lib.rs")
         .include("../../core/include")
